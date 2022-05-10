46362c286e3188c75368b13be19bf29a87e4d412
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-05-09 23:48:32 -0400

    worked on creating the template.js file under src directory and the functions to generate the html with the team members information. I have used a sample html to help write that code for the page and then copy it to the correspinding javascript file.

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const render = require('./src/template');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const employees = [];
+const teamMembers = {
+    manager: null,
+    engineers: [],
+    interns: [],
+};
 
-function generateHtml() 
+const idArray = [];
 
-// function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then((answers) => {
-        const filename = './dist/index.html';
 
-        // function to write README file
-        fs.writeFile(filename, generateHtml(answers), (err) => 
-        err ? console.log(err) : console.log("Success!")
-        );
+
+
+
+function buildTeam() {
+    fs.writeFile('dist/myteam.html', render(teamMembers), (err) => {
+        if(err) {
+            console.log(err);
+        }
     })
 }
 
+// function to initialize app
+// function init() {
+//     inquirer.prompt(questions)
+//     .then((answers) => {
+//         const filename = './dist/index.html';
+
+//         // function to write README file
+//         fs.writeFile(filename, generateHtml(answers), (err) => 
+//         err ? console.log(err) : console.log("Success!")
+//         );
+//     })
+// }
+
 // Function call to initialize app
-init();
+// init();
---
+++ src/template.js
@@ -0,0 +1,94 @@
+function createManager(manager) {
+    return `
+    <div class="card d-flex justify-content-evenly m-5 border border-4 border-info shadow employee-card" style="width: 18rem;">
+    <div class="card-header fs-2 bg-success">
+      <h2 class="card-title">${manager.getName()}</h2>
+      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush p-4">
+        <li class="list-group-item">ID: ${manager.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+        <li class="list-group-item">Office Number: officenumber</li>
+      </ul>
+    </div>
+  </div>
+  `;
+}
+
+function createEngineers(engineers) {
+    const markup = engineers.map((engineer) => {
+    return `
+    <div class="card d-flex justify-content-evenly m-5 border border-4 border-info shadow employee-card" style="width: 18rem;">
+    <div class="card-header fs-2 bg-success">
+      <h2 class="card-title">${engineer.getName()}</h2>
+      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush p-4">
+        <li class="list-group-item">ID: ${engineer.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+        <li class="list-group-item">GitHub: <a href='https://github.com/${engineer.getGitHub()}' target='_blank' rel='noopener noreferrer'>${engineer.getGitHub()}</a></li>
+      </ul>
+    </div>
+  </div>
+  `;
+    });
+    return markup.join(",");
+}
+
+function createInterns(interns) {
+    const markup = interns.map((intern) => {
+    return `
+    <div class="card d-flex justify-content-evenly m-5 border border-4 border-info shadow employee-card" style="width: 18rem;">
+    <div class="card-header fs-2 bg-success">
+      <h2 class="card-title">${intern.getName()}</h2>
+      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush p-4">
+        <li class="list-group-item">ID: ${intern.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li class="list-group-item">School: ${intern.getSchool()}</li>
+      </ul>
+    </div>
+  </div>
+  `;
+    });
+    return markup.join(",")
+}
+
+function generateTeam(teamMembers) {
+    return `
+        ${createManager(teamMembers.manager)}
+        ${createEngineers(teamMembers.engineers)}
+        ${createInterns(teamMembers.interns)}
+    `;
+}
+
+function render(teamMembers) {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Team Profile</title>
+</head>
+<body>
+    <header class="p-5 bg-danger text-white text-center fs-1 fw-bolder">My Team</header>
+    <main class="container">
+        <section class="row">
+            <section class="team-area col-12 d-flex justify-content-center">
+              ${generateTeam(teamMembers)}
+            </section>
+        </section>
+    </main>
+</body>
+</html>`;
+}
+
+
+module.exports = render;
