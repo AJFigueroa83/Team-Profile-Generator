ef3947545498f27f1709ac4a2d5c8531c735900b
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-05-11 17:14:04 -0400

    added some styling to the html.

--- src/template.js
+++ src/template.js
@@ -3,7 +3,7 @@ function createManager(manager) {
     <div class="card d-flex justify-content-evenly m-5 border border-4 border-info shadow employee-card" style="width: 18rem;">
     <div class="card-header fs-2 bg-success">
       <h2 class="card-title">${manager.getName()}</h2>
-      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+      <h3 class="card-title"><i class="fas fa-briefcase mr-2"></i>  ${manager.getRole()}</h3>
     </div>
     <div class="card-body">
       <ul class="list-group list-group-flush p-4">
@@ -22,7 +22,7 @@ function createEngineers(engineers) {
     <div class="card d-flex justify-content-evenly m-5 border border-4 border-info shadow employee-card" style="width: 18rem;">
     <div class="card-header fs-2 bg-success">
       <h2 class="card-title">${engineer.getName()}</h2>
-      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+      <h3 class="card-title"><i class="fas fa-hammer"></i>  ${engineer.getRole()}</h3>
     </div>
     <div class="card-body">
       <ul class="list-group list-group-flush p-4">
@@ -43,7 +43,7 @@ function createInterns(interns) {
     <div class="card d-flex justify-content-evenly m-5 border border-4 border-info shadow employee-card" style="width: 18rem;">
     <div class="card-header fs-2 bg-success">
       <h2 class="card-title">${intern.getName()}</h2>
-      <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+      <h3 class="card-title"><i class="fas fa-graduation-cap"></i>  ${intern.getRole()}</h3>
     </div>
     <div class="card-body">
       <ul class="list-group list-group-flush p-4">
@@ -75,10 +75,11 @@ function render(teamMembers) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
     <title>Team Profile</title>
 </head>
 <body>
-    <header class="p-5 bg-danger text-white text-center fs-1 fw-bolder">My Team</header>
+    <header class="p-5 bg-danger text-white text-center fs-1 fw-bolder" id="header" style="background: linear-gradient(50deg, red, blue)">My Team</header>
     <main class="container">
         <section class="row">
             <section class="team-area col-12 d-flex justify-content-center">
